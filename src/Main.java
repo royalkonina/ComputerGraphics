f3a842c6a663a45e142d0ae256e08bb0c6f0356b
AusevEgor <ausev5egor@gmail.com>
2018-03-30 01:24:41 +0400

    Added drawing images using default, Bresenham, Wu algorithms.

--- src/Main.java
+++ src/Main.java
@@ -1,15 +1,17 @@
 import geometric_primitives.Model;
+import utils.Drawer;
 import utils.ObjectReader;
 
 import java.io.IOException;
 
 public class Main {
 
-    public static final String FILENAME = "input_output_files/african_head.obj";
+    public static final String FILENAME = "/home/egor/IdeaProjects/Com_grap_lab_1/src/input_output_files/african_head.obj";
 
     public static void main(String[] args) throws IOException {
 
         ObjectReader reader = new ObjectReader(FILENAME);
         Model model = reader.readModel();
+        Drawer.draw(model);
     }
 }
