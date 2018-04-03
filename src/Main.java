9680f69d1e1f41c27c0666888baf7316c24cf71e
AusevEgor <ausev5egor@gmail.com>
2018-04-03 13:36:48 +0400

    Added texturing, loading tga image from file. And a bit refactoring

--- src/Main.java
+++ src/Main.java
@@ -1,20 +1,28 @@
 import geometric_primitives.Model;
 import geometric_primitives.PointDouble;
+import objects.TgaImage;
 import utils.Drawer;
 import utils.ObjectReader;
+import utils.PictureIO;
 
 import java.io.IOException;
 
 public class Main {
 
     public static final String FILENAME = "/home/egor/IdeaProjects/Com_grap_lab_1/src/input_output_files/african_head.obj";
+    private static final String TEXTURE_FILENAME = "/home/egor/IdeaProjects/Com_grap_lab_1/src/input_output_files/african_head_diffuse.tga";
+
 
     public static void main(String[] args) throws IOException {
 
-        ObjectReader reader = new ObjectReader(FILENAME);
-        Model model = reader.readModel();
-        //Drawer.draw(model);
+        ObjectReader objReader = new ObjectReader(FILENAME);
+
+        TgaImage texture = PictureIO.readImage(TEXTURE_FILENAME, "texture");
+
         PointDouble camera = new PointDouble(0, 0, 1);
+        Model model = objReader.readModel();
+        //Drawer.draw(model);
         Drawer.drawWithBarycentric(model, camera);
+        Drawer.drawWithTexture(model, camera, texture);
     }
 }
